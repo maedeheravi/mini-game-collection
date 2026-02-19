75bb87b8544351a021a9d2ae0f0fa53acc7e4c20
fatemefazeli812-ctrl <fatemefazeli812@gmail.com>
2026-02-19 11:24:58 -0800

    Update MainMenu.cs

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -3,34 +3,35 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // رفتن به بازی اول
+    
     public void PlayGame1()
     {
         SceneManager.LoadScene("game1");  // اسم صحنه بازی اول
     }
     
-    // رفتن به بازی دوم
+    
     public void PlayGame2()
     {
         SceneManager.LoadScene("game2");  // اسم صحنه بازی دوم
     }
     
-    // رفتن به بازی سوم
+    
     public void PlayGame3()
     {
         SceneManager.LoadScene("game3.2");  // اسم صحنه بازی سوم
     }
     
-    // رفتن به بازی چهارم
+    
     public void PlayGame4()
     {
         SceneManager.LoadScene("game4");  // اسم صحنه بازی چهارم
     }
     
-    // برگشت به منوی اصلی
+    
     public void BackToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");  // اسم صحنه منوی اصلی
         Debug.Log("بازگشت به منو");
     }
+
 }
